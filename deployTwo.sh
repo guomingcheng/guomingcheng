aeb008123740da16e2c64561617fa6e0fb858545
guomingcheng <18476308746@163.com>
2020-10-04 23:16:05 +0800

    deplayTwo

---
+++ deployTwo.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+rm -fr docs/.vuepress/dist
+
+git add -A
+
+git commit -m 'deplayTwo'
+
+git push -f git@github.com:guomingcheng/Texter2.git master
+
+
+cd -
